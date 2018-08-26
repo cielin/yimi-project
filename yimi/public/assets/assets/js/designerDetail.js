2a9b25461f111fcc73edb5bc8c192fd68526d88a
cyrial <racharne@gmail.com>
2018-08-26 16:25:17 +0800

    删除多余的assets文件夹

--- yimi/public/assets/assets/js/designerDetail.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$("#littleImg").find("img").click(function(){
-		
-		var bigImgSrc = $(this).siblings("input").val();
-		$("#bigImg").attr("src",bigImgSrc);
-
-	})
-})
--- yimi/public/assets/assets/js/initSupplier.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$('.sub-company-btn').popover();
-	$(".supplier-company").find("li").click(function(){
-		$("#myModal").show()
-	})
-})
-
