b22ade31a59351814610b6fe96dd3174adb0cf37
wangxiuling <wangxiuling@travelsky.com>
2018-10-26 13:36:40 +0800

    提交修改

--- yimi/resources/views/brands/show.blade.php
+++ yimi/resources/views/brands/show.blade.php
@@ -136,9 +136,10 @@
 
 @section('js')
 <!-- Waypoints -->
-<script src="{{ URL::asset('../plugin/jquery.waypoints.min.js"></script>
+<script type="text/javascript" src="{{ URL::asset('../plugin/jquery.waypoints.min.js') }}"></script>
+
 <!-- Magnific Popup -->
-<script src="{{ URL::asset('../plugin/jquery.magnific-popup.min.js"></script>
+<script type="text/javascript" src="{{ URL::asset('../plugin/jquery.magnific-popup.min.js') }}"></script>
 <!-- Salvattore -->
-<script src="{{ URL::asset('../plugin/salvattore.min.js"></script>
+<script type="text/javascript" src="{{ URL::asset('../plugin/salvattore.min.js') }}"></script>
 @stop
