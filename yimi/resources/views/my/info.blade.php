6481baa2fc80a6272c0a5e2dd090f15764731fe6
wangxiuling <wangxiuling@travelsky.com>
2018-10-30 09:51:07 +0800

    提交修改

--- yimi/resources/views/my/info.blade.php
+++ yimi/resources/views/my/info.blade.php
@@ -89,8 +89,8 @@
                     <?php
                     echo "今天是 " . date("Y-m-d") . "<br>";
                     echo "今ss " . formatDate("Y-m-d","2012-11-03 00:46:22" , 1) . "<br>";
-                    echo "今ss " . $user->birthday . "<br>";
-                    echo "今三生三世 " . formatDate("Y-m-d",$user->birthday, 1) . "<br>";
+                    echo "今ss " . "{{ $user->birthday }}" . "<br>";
+                    echo "今三生三世 " . formatDate("Y-m-d","{{ $user->birthday }}", 1) . "<br>";
                     ?>
                 </div>
                 <button type="submit" class="address-btn">保存</button>
