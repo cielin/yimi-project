5ec7315987f1155e6fb5464d981c120a52feaa0f
wangxiuling <wangxiuling@travelsky.com>
2018-11-11 18:54:46 +0800

    提交修改

--- yimi/resources/views/designers/index.blade.php
+++ yimi/resources/views/designers/index.blade.php
@@ -18,7 +18,26 @@
     </div>
     <!--面包屑-->
     @if (isset($designers) && sizeof($designers) > 0)
-
+    @foreach ($designers as $designer)
+    <div class="row designer-item">
+        <div class="col-md-6 col-sm-12">
+            <div class="designer-bg clearfix">
+                <div class="designer-user col-md-5  col-sm-1">
+                    <a href="{{ URL::to('/designers/' . $designer->slug) }}"><img src="{{ asset('public/thumbs/designers/thumb_' . $designer->avatar) }}"></a>
+                </div>
+                <div class="designer-text col-md-7 col-sm-11">
+                    <h5><a href="{{ URL::to('/designers/' . $designer->slug) }}"><span>{{ $designer->name }}</span></a></h5>
+                    <p><a href="{{ URL::to('/designers/' . $designer->slug) }}">{{ mb_substr(strip_tags($designer->description), 0, 90) }}</a></p>
+                    <div class="more"><a href="{{ URL::to('/designers/' . $designer->slug) }}">MORE <i class="icon iconfont icon-shuangjiantou"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-12 designer_list">
+            图
+        </div>
+    </div>
+    @endforeach
     <nav class="clearfix" aria-label="page navigation">
         <?php echo $designers->links(); ?>
     </nav>
