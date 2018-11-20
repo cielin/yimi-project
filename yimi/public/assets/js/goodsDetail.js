4292fa2bbd0a14d45d73ecad388e6d4fd6c5c5a3
wangxiuling <wangxiuling@travelsky.com>
2018-11-20 19:54:53 +0800

    tij xiug

--- yimi/public/assets/js/goodsDetail.js
+++ yimi/public/assets/js/goodsDetail.js
@@ -49,20 +49,9 @@ $(function () {
   var swiper = new Swiper('.swiperPro', {
       slidesPerView: 1,
       spaceBetween: 30,
-      // paginationClickable: true,
-      // loop: true,
-      // speed: 1000,
-      // autoplay: {
-      //   delay: 2500,
-      //   disableOnInteraction: false,
-      // },
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
       }
     });
  
