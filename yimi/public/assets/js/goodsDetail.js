c0fea6d70ce00b5af261d997db771346680ede7d
wangxiuling <wangxiuling@travelsky.com>
2018-11-20 19:48:58 +0800

    tij xiug

--- yimi/public/assets/js/goodsDetail.js
+++ yimi/public/assets/js/goodsDetail.js
@@ -45,10 +45,33 @@ $(function () {
          });
 
 
-     var swiper2 = new Swiper('.swiper-container', {
+     
+  var swiper = new Swiper('.swiper1', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      // paginationClickable: true,
+      loop: true,
+      speed: 1000,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
       pagination: {
         el: '.swiper-pagination',
+        clickable: true,
       },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      on: {
+      slideChange: function () {
+        var imgWidth = 1920;
+        var boxWidth = this.width;
+        var left = (-(imgWidth-boxWidth)/2)+"px";
+        
+      },
+    }
     });
  
 	})
--- yimi/resources/views/products/show.blade.php
+++ yimi/resources/views/products/show.blade.php
@@ -69,6 +69,27 @@
 
             </div>
         </div>
+        <div class="sowingMap">
+        <div class="swiper-container swiper1">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide">Slide 1</div>
+              <div class="swiper-slide">Slide 2</div>
+              <div class="swiper-slide">Slide 3</div>
+              <div class="swiper-slide">Slide 4</div>
+              <div class="swiper-slide">Slide 5</div>
+              <div class="swiper-slide">Slide 6</div>
+              <div class="swiper-slide">Slide 7</div>
+              <div class="swiper-slide">Slide 8</div>
+              <div class="swiper-slide">Slide 9</div>
+              <div class="swiper-slide">Slide 10</div>
+            </div>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination"></div>
+            <!-- Add Arrows -->
+            <!-- <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div> -->
+        </div>
+    </div>
     <div class="swiper-container"  id="mobileWraper">
         <div class="swiper-wrapper">
           <div class="swiper-slide">Slide 1</div>
